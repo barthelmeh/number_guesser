12fb689e179755024f660d61d1e64b29d75e4aba
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2023-10-30 12:12:26 +1300

    Speeding up network training using back prop

--- Layer.py
+++ Layer.py
@@ -6,13 +6,56 @@ class Layer:
         self.num_nodes_out = num_nodes_out
 
         self.weights = [[0 for _ in range(self.num_nodes_in)] for _ in range(self.num_nodes_out)]
+        self.costGradientW = [[0 for _ in range(self.num_nodes_in)] for _ in range(self.num_nodes_out)]
         self.biases = [0 for _ in range(self.num_nodes_out)]
+        self.costGradientB = [0 for _ in range(self.num_nodes_out)]
+
+        self.stored_activations = [0 for _ in range(self.num_nodes_out)]
+        self.stored_weighted_inputs = [0 for _ in range(self.num_nodes_out)]
+        self.stored_inputs = []
+
+        self.randomize_weights()
+
+    def randomize_weights(self):
+
+        for out_node in range(self.num_nodes_out):
+
+            for in_node in range(self.num_nodes_in):
+                
+                val = random.uniform(-1, 1)
+                self.weights[out_node][in_node] = val
+
+    def clear_gradients(self):
+        self.costGradientW = [[0 for _ in range(self.num_nodes_in)] for _ in range(self.num_nodes_out)]
+        self.costGradientB = [0 for _ in range(self.num_nodes_out)]
+
+
 
     def activation_function(self, weighted_input):
         """
         sigmoid function
         """
         return 1 / (1 + math.exp(-weighted_input))
+    
+    def activation_derivative(self, weighted_input):
+        activation = self.activation_function(weighted_input)
+        return activation * (1 - activation)
+    
+    def apply_gradients(self, learn_rate):
+
+        for out_node in range(self.num_nodes_out):
+
+            self.biases[out_node] -= self.costGradientB[out_node] * learn_rate
+            for in_node in range(self.num_nodes_in):
+                self.weights[out_node][in_node] = self.costGradientW[out_node][in_node] * learn_rate
+    
+    def node_cost(self, output_activation, expected_output):
+        cost = output_activation - expected_output
+        return cost * cost
+    
+    def node_cost_derivative(self, output_activation, expected_output):
+        return 2 * (output_activation - expected_output)
+    
 
     def calculate_outputs(self, inputs):
         activations = []
@@ -20,12 +63,53 @@ class Layer:
         for out_node in range(self.num_nodes_out):
             weighted_input = self.biases[out_node]
             for in_node in range(self.num_nodes_in):
-                weighted_input += inputs[in_node] * self.weights[in_node][out_node]
+                weighted_input += inputs[in_node] * self.weights[out_node][in_node]
 
-            activations.append(self.activation_function(weighted_input))
+            activation_value = self.activation_function(weighted_input)
+            activations.append(activation_value)
 
+            self.stored_weighted_inputs[out_node] = weighted_input
+            self.stored_activations[out_node] = activation_value
+
+        self.stored_inputs = inputs[:]
         return activations
+    
+    def calculate_output_layer_node_values(self, expected_outputs):
+        node_values = []
 
-    def node_cost(self, output_activation, expected_output):
-        cost = expected_output - output_activation
-        return cost * cost
+        for i in range(len(expected_outputs)):
+            # Calculate the partial derivatives: cost/activation and activation/weighted_input
+            cost_derivative = self.node_cost_derivative(self.stored_activations[i], expected_outputs[i])
+            activation_derivative = self.activation_derivative(self.stored_weighted_inputs[i])
+            node_values.append(activation_derivative * cost_derivative)
+
+        return node_values
+    
+    def calculate_hidden_layer_node_values(self, oldLayer, old_node_values):
+        new_node_values = []
+
+        for new_node_index in range(self.num_nodes_out):
+            new_node_value = 0
+            
+            for old_node_index in range(len(old_node_values)):
+
+                weight_derivative = oldLayer.weights[old_node_index][new_node_index]
+                new_node_value += weight_derivative * old_node_values[old_node_index]
+
+            new_node_value *= self.activation_derivative(self.stored_weighted_inputs[new_node_index])
+            new_node_values.append(new_node_value)
+
+        return new_node_values
+    
+    def update_gradients(self, node_values):
+
+        for out_node in range(self.num_nodes_out):
+
+            for in_node in range(self.num_nodes_in):
+
+                # Calculate partial derivative: cost/weight
+                weight_derivative = self.stored_inputs[in_node] * node_values[out_node]
+                self.costGradientW[out_node][in_node] += weight_derivative
+
+            bias_derivative = 1 * node_values[out_node]
+            self.costGradientB[out_node] += bias_derivative
--- Network.py
+++ Network.py
@@ -33,8 +33,44 @@ class Network:
         for data_point in data_points:
             total_cost += self.calculate_cost(data_point)
         return total_cost / len(data_points)
+    
 
-    def train(self, input_data, epochs):
-        pass
+    # Performs one iteration of gradient descent
+    def learn(self, input_data, learn_rate):
+       
+        for data_point in input_data:
+           self.update_all_gradients(data_point)
 
+        self.apply_all_gradients(learn_rate / len(input_data))
 
+        # Set back to zero
+        self.clear_all_gradients()
+       
+
+    def apply_all_gradients(self, learn_rate):
+        """
+        Applies the gradients on all the layers
+        """
+
+        for layer in self.layers:
+            layer.apply_gradients(learn_rate)
+
+    def clear_all_gradients(self):
+        for layer in self.layers:
+            layer.clear_gradients()
+
+    # Backwards Propogation
+    def update_all_gradients(self, data_point: DataPoint):
+        # Run all the inputs through the network
+        self.calculate_outputs(data_point.inputs)
+
+        # Update gradients of output layer
+        output_layer = self.layers[len(self.layers) - 1]
+        node_values = output_layer.calculate_output_layer_node_values(data_point.expected_outputs)
+        output_layer.update_gradients(node_values)
+
+        # Update gradients of hidden layers
+        for hidden_layer_index in range(len(self.layers)-2, 0, -1):
+            hidden_layer = self.layers[hidden_layer_index]
+            hidden_node_values = hidden_layer.calculate_hidden_layer_node_values(self.layers[hidden_layer_index + 1], node_values)
+            hidden_layer.update_gradients(hidden_node_values)
